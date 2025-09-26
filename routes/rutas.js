1b349060b5d7fd79fb8a24c31a05e11c1abe736f
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2025-09-25 19:03:52 -0600

    Add files via upload

---
+++ routes/rutas.js
@@ -0,0 +1,14 @@
+import{Router} from "express"
+const router = Router()
+router.get("/", function(req, res){
+	res.render("home")
+})
+
+router.get("/info",function(req, res){
+	res.render("información")
+})
+
+router.get("/contactanos",function(req, res){
+	res.render("contactanos")
+})
+export default router
